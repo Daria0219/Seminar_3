235e4d0960c7112565c5175ebe3d15b7ccf3854b
Your Name <you@example.com>
2022-10-28 20:21:05 +0400

    решили задачу на палиндром

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*void PrintArray (int[] arr)
+{   int count = arr.Length;
+    int pos=0;
+    while (pos<count)
+    {Console.WriteLine(arr[pos]);
+     pos++;}
+}*/
+
+// 1. программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+void Palindrom(int number){
+int firstN = number/10000;
+int SecondN = (number/1000)%10;
+int forthN = (number/10)%10;
+int fifthN = number%10;
+if (firstN==fifthN && SecondN==forthN) Console.WriteLine("да, это палиндром");
+else Console.WriteLine("это не палиндром");
+}
+Console.WriteLine("Введите пятизначное число");
+int N = int.Parse(Console.ReadLine());
+Palindrom(N);
+
