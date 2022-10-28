36a19e981c40c98be19dc5346623c8c7fed3e37b
Your Name <you@example.com>
2022-10-28 20:34:29 +0400

    решили вторую задачу

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
      pos++;}
 }*/
 
-// 1. программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+/* 1. программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
 
 void Palindrom(int number){
 int firstN = number/10000;
@@ -19,4 +19,21 @@ else Console.WriteLine("это не палиндром");
 Console.WriteLine("Введите пятизначное число");
 int N = int.Parse(Console.ReadLine());
 Palindrom(N);
+*/
+
+// 2. программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+
+double Distance(int x1, int y1, int z1, int x2, int y2, int z2){
+double dist = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
+return dist;}
+Console.WriteLine("Введите координаты точки А");
+int X = int.Parse(Console.ReadLine());
+int Y = int.Parse(Console.ReadLine());
+int Z = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B");
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int C = int.Parse(Console.ReadLine());
+double otvet = Distance(X,Y,Z,A,B,C);
+Console.WriteLine(otvet);
 
